962ecee14c47c22b91fe75475ac38f3179ba386d
njgarg22 <njgarg22@gmail.com>
2020-08-04 23:07:40 +0530

    Rename TopicService.java to TopicServiceWithoutDB.java

--- src/main/java/com/neeraj/springbootstarter/Topic/TopicService.java
+++ src/main/java/com/neeraj/springbootstarter/Topic/TopicService.java
@@ -1,45 +1,5 @@
 package com.neeraj.springbootstarter.Topic;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-@Service
 public class TopicService {
-	
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic("spring", "Spring framework", "Spring framework descr"),
-			new Topic("java", "Core Java", "Core Java descr"),
-			new Topic("js", "JavaScript", "JavaScript descr")
-			));
-	
-	public List<Topic> getAllTopics(){
-		return topics;
-	}
-	
-	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-	}
-	
-	public Topic addTopic(Topic topic) {
-		topics.add(topic);
-		return topic;
-	}
-
-	public Topic updateTopic(String id, Topic topic) {
-		for(int i = 0; i < topics.size(); i++) {
-			if(topics.get(i).getId().equals(id)) {
-				topics.set(i, topic);
-				break;
-			}
-		}
-		return topic;
-	}
-
-	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
-	}
 
 }
