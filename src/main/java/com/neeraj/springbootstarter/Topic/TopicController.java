b98109c87201db98ef3dd703df9e148e57230d02
njgarg22 <njgarg22@gmail.com>
2020-08-04 18:27:11 +0530

    Add API endpoint for single topic

--- src/main/java/com/neeraj/springbootstarter/Topic/TopicController.java
+++ src/main/java/com/neeraj/springbootstarter/Topic/TopicController.java
@@ -3,6 +3,7 @@ package com.neeraj.springbootstarter.Topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class TopicController {
 	@RequestMapping("/topics")
 	public List<Topic> getTopics(){
 		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{foo}")
+	public Topic getTopic(@PathVariable("foo") String id) {
+		return topicService.getTopic(id);
 	}
 }
